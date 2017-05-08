9a66e739a724b73363ca0573a02bb7fcacd410d9
Vivian Lobo <Vivian.Lobo@ca.com>
2017-05-08 14:40:05 +0100

    Cleanup and Sorting

---
+++ Cppcheck-VS-AddIn/VSPackage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Cppcheck_VS_AddIn
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]       
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidCppcheckCommandPackageString)]
+    public sealed class VSPackage : Package
+    {
+
+        public VSPackage()
+        {
+        }
+
+        protected override void Initialize()
+        {
+            CppcheckCommand.Initialize(this);
+            base.Initialize();
+        }
+
+    }
+}
